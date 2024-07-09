ff4dfc2a1e99440fe86b1a9d760e858bea59f75c
Daniel Gomez <daniel.gomez@viewnext.com>
2024-07-09 12:42:46 +0200

    Initial commit

---
+++ formacion/src/test/java/es/seg_social/formacion/FormacionApplicationTests.java
@@ -0,0 +1,13 @@
+package es.seg_social.formacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
