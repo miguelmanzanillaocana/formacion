0701e7d558fa376dce000d43b452eeb571c57c17
Daniel Gomez <daniel.gomez@viewnext.com>
2024-07-11 12:09:02 +0200

    Arreglar angular

---
+++ formacion-web/src/app/datos.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatosService {
+
+  private url = 'http://localhost:8080/aplicacion/all'
+
+  constructor(private http: HttpClient) { }
+
+  obtenerDatos(): Observable<any[]> {
+    return this.http.get<any[]>(this.url);
+  }
+}
---
+++ formacion-web/src/app/tabla-datos/tabla-datos/tabla-datos.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { DatosService } from '../../datos.service';
+
+@Component({
+  selector: 'app-tabla-datos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabla-datos.component.html',
+  styleUrl: './tabla-datos.component.css'
+})
+
+export class TablaDatosComponent {
+  
+  datos: any[]=[];
+
+  constructor(private datosService: DatosService){  }
+
+  ngOnInit(): void{
+    this.datosService.obtenerDatos().subscribe((datos: any[]) => {
+      this.datos = datos as any[];
+    })
+  }
+}
