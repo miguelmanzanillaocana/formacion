20b59daa3100d9da4aa9340ade639c97627ab6db
miguelmanzanillaocana <164020585+miguelmanzanillaocana@users.noreply.github.com>
2024-07-19 11:38:25 +0200

    DIALOGO CONFIRMAR

---
+++ formacion-web/src/app/aplicaciones/tabla-datos/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [MatDialogModule],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.css'
+})
+export class ConfirmDialogComponent {
+  codigo: string ='';
+
+  constructor(private dialogRef: MatDialogRef<ConfirmDialogComponent>) { }
+
+  onClose(confirm: boolean) {
+    this.dialogRef.close(confirm);
+  }
+}
--- formacion-web/src/app/aplicaciones/tabla-datos/tabla-datos.component.ts
+++ formacion-web/src/app/aplicaciones/tabla-datos/tabla-datos.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DatosService } from '../../datos.service';
 import { IAplicacion } from '../../interfaces/AplicacionInterface';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 @Component({
   selector: 'app-tabla-datos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatDialogModule],
   templateUrl: './tabla-datos.component.html',
   styleUrl: './tabla-datos.component.css'
 })
@@ -14,20 +15,36 @@ import { IAplicacion } from '../../interfaces/AplicacionInterface';
 export class TablaDatosComponent {
   datos: IAplicacion[]=[];
 
-  constructor(private datosService: DatosService){  }
+  constructor(private datosService: DatosService, private dialog: MatDialog){  }
 
   ngOnInit(): void{
+    this.actualizarTabla();
+  }
+
+  actualizarTabla(){
     this.datosService.obtenerAplicaciones().subscribe((datos: IAplicacion[]) => {
       this.datos = datos as IAplicacion[];
     })
   }
+
   borrarAplicacion(cod: string) {
-    this.datosService.borrarAplicacion(cod).subscribe((resultado) => {
-      if (resultado) {
-        console.log('Aplicación eliminada con éxito');
-      } else {
-        console.log('Error al eliminar la aplicación');
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '500px',
+      data: { codigo: cod }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.datosService.borrarAplicacion(cod).subscribe((resultado) => {
+          if (resultado) {
+            this.actualizarTabla();
+            console.log('Aplicación eliminada con éxito');
+          } else {
+            console.log('Error al eliminar la aplicación');
+          }
+        });
       }
     });
   }
+
 }
