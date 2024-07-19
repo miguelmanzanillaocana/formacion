4d0cead556952046ee2734e833f273b76a844405
Daniel Gomez <daniel.gomez@viewnext.com>
2024-07-19 12:51:53 +0200

    Añadida directiva de ordenar

--- formacion-web/src/app/aplicaciones/tabla-datos/tabla-datos.component.ts
+++ formacion-web/src/app/aplicaciones/tabla-datos/tabla-datos.component.ts
@@ -1,50 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DatosService } from '../../datos.service';
-import { IAplicacion } from '../../interfaces/AplicacionInterface';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { Aplicacion } from '../../classes/aplicacion';
+import { SortDirective } from '../../directive/sort.directive';
+
 @Component({
   selector: 'app-tabla-datos',
   standalone: true,
-  imports: [CommonModule,MatDialogModule],
+  imports: [CommonModule, SortDirective],
   templateUrl: './tabla-datos.component.html',
   styleUrl: './tabla-datos.component.css'
 })
 
 export class TablaDatosComponent {
-  datos: IAplicacion[]=[];
+  datos: Aplicacion[]=[];
+aplicaciones: any;
 
-  constructor(private datosService: DatosService, private dialog: MatDialog){  }
+  constructor(private datosService: DatosService){  }
 
   ngOnInit(): void{
-    this.actualizarTabla();
-  }
-
-  actualizarTabla(){
-    this.datosService.obtenerAplicaciones().subscribe((datos: IAplicacion[]) => {
-      this.datos = datos as IAplicacion[];
+    this.datosService.obtenerAplicaciones().subscribe((datos: Aplicacion[]) => {
+      this.datos = datos as Aplicacion[];
     })
   }
-
   borrarAplicacion(cod: string) {
-    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-      width: '500px',
-      data: { codigo: cod }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.datosService.borrarAplicacion(cod).subscribe((resultado) => {
-          if (resultado) {
-            this.actualizarTabla();
-            console.log('Aplicación eliminada con éxito');
-          } else {
-            console.log('Error al eliminar la aplicación');
-          }
-        });
+    this.datosService.borrarAplicacion(cod).subscribe((resultado) => {
+      if (resultado) {
+        console.log('Aplicación eliminada con éxito');
+      } else {
+        console.log('Error al eliminar la aplicación');
       }
     });
   }
-
 }
