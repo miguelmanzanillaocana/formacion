b2223a27c2baa915880cb9c03f9c9bcd85505e5d
Daniel Gomez <daniel.gomez@viewnext.com>
2024-07-19 13:21:20 +0200

    Filtro por codigo de aplicacion

--- formacion-web/src/app/aplicaciones/tabla-datos/tabla-datos.component.ts
+++ formacion-web/src/app/aplicaciones/tabla-datos/tabla-datos.component.ts
@@ -4,26 +4,44 @@ import { DatosService } from '../../datos.service';
 import { Aplicacion } from '../../classes/aplicacion';
 import { SortDirective } from '../../directive/sort.directive';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { map, Observable, startWith, withLatestFrom } from 'rxjs';
 
 @Component({
   selector: 'app-tabla-datos',
   standalone: true,
-  imports: [CommonModule, SortDirective, RouterLink, RouterOutlet],
+  imports: [CommonModule, SortDirective, RouterLink, RouterOutlet, ReactiveFormsModule],
   templateUrl: './tabla-datos.component.html',
   styleUrl: './tabla-datos.component.css'
 })
 
 export class TablaDatosComponent {
-  datos: Aplicacion[]=[];
-aplicaciones: any;
+datosTabla: Aplicacion[]=[];
+datos: Observable<Aplicacion[]>;
+datosFiltrados: Observable<Aplicacion[]>;
 
-  constructor(private datosService: DatosService){  }
+formGroup: FormGroup;
 
-  ngOnInit(): void{
-    this.datosService.obtenerAplicaciones().subscribe((datos: Aplicacion[]) => {
-      this.datos = datos as Aplicacion[];
+  constructor(private datosService: DatosService, private fb: FormBuilder){ 
+    this.formGroup = fb.group({ filter: ['']})
+
+    this.datos = this.datosService.obtenerAplicaciones();
+
+    this.datosFiltrados = this.formGroup.get('filter')!.valueChanges.pipe(
+      startWith(''),
+      withLatestFrom(this.datos),
+      map(([val, datos]) => !val ? datos : datos.filter((x) => x.codAplic.toLowerCase().includes(val)))
+    )
+
+    this.datosFiltrados.subscribe((datos: Aplicacion[]) => {
+      this.datosTabla = datos as Aplicacion[];
     })
   }
+
+  ngOnInit(): void{
+    
+  }
+
   borrarAplicacion(cod: string) {
     this.datosService.borrarAplicacion(cod).subscribe((resultado) => {
       if (resultado) {
