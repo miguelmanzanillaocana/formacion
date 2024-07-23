49d6a692a5ac79d378a5c481509fccd35d12a85f
Daniel Gomez <daniel.gomez@viewnext.com>
2024-07-23 10:30:27 +0200

    Eliminado home

--- formacion-web/src/app/app.routes.ts
+++ formacion-web/src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 
 import { AplicacionesComponent } from './aplicaciones';
 import { SituacionesComponent } from './situaciones';
-import { HomeComponent } from './home';
 import { CrearAplicacionComponent } from './crear-aplicacion';
 
 export const routes: Routes = [
     { path: 'aplicaciones', component: AplicacionesComponent },
     { path: 'situaciones', component: SituacionesComponent },
-    { path: 'inicio', component: HomeComponent},
     { path: 'crear-app', component: CrearAplicacionComponent},
-    { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+    { path: '', redirectTo: '/aplicaciones', pathMatch: 'full' }
     
 ];
