70ef444883700c817af14d2b34047cf220791f10
Daniel Gomez <daniel.gomez@viewnext.com>
2024-08-01 14:37:57 +0200

    Cambio servicio aplicacion

--- formacion/src/main/java/es/seg_social/formacion/services/AplicacionService.java
+++ formacion/src/main/java/es/seg_social/formacion/services/AplicacionService.java
@@ -33,8 +33,8 @@ public class AplicacionService {
 		if (!repository.findById(id).get().equals(Optional.empty())) {
 			
 			situacionService.deleteSituacionById(id);
-			
 			repository.deleteById(id);
+			
 			respuesta = true;
 		}
 		
