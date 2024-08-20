f245cdc5384e6e66d1afdeb5981a2ec3d49b4230
Daniel Gomez <daniel.gomez@viewnext.com>
2024-08-20 12:12:25 +0200

    Creado generador de rsakey y terminado autenticacion

---
+++ formacion/src/main/java/es/seg_social/formacion/KeyGenerator.java
@@ -0,0 +1,49 @@
+package es.seg_social.formacion;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+public class KeyGenerator {
+	
+	private RSAPublicKey pubKey;
+	private RSAPrivateKey priKey;
+	
+	public KeyGenerator() {
+		this.generate();
+	}
+	
+	public final void generate() {
+		try {
+			KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+			gen.initialize(4096);
+			
+			KeyPair pair = gen.generateKeyPair();
+			
+			this.setPublic((RSAPublicKey) pair.getPublic());
+			this.setPrivate((RSAPrivateKey) pair.getPrivate());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void setPublic(RSAPublicKey pub) {
+		this.pubKey = pub;
+	}
+	
+	public RSAPublicKey getPublic() {
+		return pubKey;
+	}
+	
+	private void setPrivate(RSAPrivateKey pri) {
+		this.priKey = pri;
+	}
+	
+	public RSAPrivateKey getPrivate() {
+		return priKey;
+	}
+
+}
